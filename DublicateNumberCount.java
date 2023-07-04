3e17d148df3a225d5393a299c0eda7a7f89447fb
sathishTask <sathish.rathinam35@gmail.com>
2023-07-04 16:40:30 +0530

    Implemented the code to find the number of pair

--- DublicateNumberCount.java
+++ DublicateNumberCount.java
@@ -11,16 +11,25 @@ public class DublicateNumberCount {
 		System.out.println(map.get(5));
 		System.out.println(map.containsKey(5));
 		System.out.println(map);*/
-		
-		int a[] = { 1, 2, 5, 3, 4, 5, 4, 5, 2, 0, 1, 6, 2, 5, 1, 7 };
+		int a[] = { 1, 2, 5, 3, 4, 5, 4, 5, 2, 0, 1, 6, 2, 5, 1, 0};
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < a.length; i++) {
 			if (map.containsKey(a[i])) {
-				map.put(a[i], map.get(a[i]) + 1);
+				map.put(a[i], map.get(a[i])+1);
 			} else {
 				map.put(a[i], 1);
 			}
 		}
 		System.out.println(map);
+		System.out.println("======================");
+		for(int j = 0 ; j<map.size(); j++) {
+			int count = map.get(j);
+			count= count/2;
+			map.put(j, count);
+			//System.out.println(j +"----"+count);
+			//System.out.println(map);
+		}
+		System.out.println(map);
+		
 	}
 }
